4d6edd5f53a0ed554becbacd093f3013357a3c63
Alan Rubin <alan.rubin@wehi.edu.au>
2019-12-10 10:27:24 +1100

    added extra reverse complement tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,8 +7,14 @@ class TestReverseComplement(unittest.TestCase):
     def test_single_nt(self):
         self.assertEqual("A", reverse_complement("T"))
 
-    def test_non_dna_character(self):
+    def test_multiple_nt(self):
+        self.assertEqual("TTCC", reverse_complement("GGAA"))
+        self.assertEqual("ACGT", reverse_complement("ACGT"))
+
+    def test_non_dna_characters(self):
         self.assertEqual("1", reverse_complement("1"))
+        self.assertEqual("4321", reverse_complement("1234"))
+        self.assertEqual("GT4321", reverse_complement("1234AC"))
 
 
 class TestConvertRnaToDNA(unittest.TestCase):
