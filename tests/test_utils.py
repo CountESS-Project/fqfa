a70f279da6f34fe09d7318f53077a27930862377
Alan Rubin <alan.rubin@wehi.edu.au>
2019-12-10 10:29:01 +1100

    added extra convert rna tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -21,3 +21,8 @@ class TestConvertRnaToDNA(unittest.TestCase):
     def test_single_nt(self):
         self.assertEqual("T", convert_rna_to_dna("U"))
         self.assertEqual("A", convert_rna_to_dna("A"))
+
+    def test_multiple_nt(self):
+        self.assertEqual("ACGT", convert_rna_to_dna("ACGU"))
+        self.assertEqual("AATT", convert_rna_to_dna("AAUU"))
+        self.assertEqual("GGAA", convert_rna_to_dna("GGAA"))
