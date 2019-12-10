960170f9a804c1a9d026648e28467e3a56244c1e
Alan Rubin <alan.rubin@wehi.edu.au>
2019-12-10 12:18:13 +1100

    added stub for translation table

---
+++ fqfa/constants/translation/table.py
@@ -0,0 +1,18 @@
+"""Constants defining a standard amino acid translation table.
+
+"""
+
+from typing import Dict
+
+CODON_TABLE: Dict[str, str] = {
+    # TODO
+}
+"""Dict[str, str]: Map from codons to single-letter amino acid codes. Sorted by codon.
+
+.. csv-table:: 
+   :header: "Codon", "Symbol", "Amino Acid"
+   :widths: 5, 5, 10
+
+    "TODO", "TODO", "TODO"
+
+"""
