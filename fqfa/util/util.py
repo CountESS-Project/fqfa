d66c0bbfb0097ddb750f41063695963e44963c98
Alan Rubin <alan.rubin@wehi.edu.au>
2019-12-12 15:25:42 +1100

    implemented NCBI translation table parser

--- fqfa/util/util.py
+++ fqfa/util/util.py
@@ -2,6 +2,8 @@
 
 """
 
+import itertools
+
 from typing import Dict, Tuple, Optional
 
 from fqfa.constants.iupac.dna import DNA_COMPLEMENTS
@@ -103,3 +105,83 @@ def translate_dna(
             raise KeyError(f"unrecognized codon '{codon}' at nt position {i + 1}")
 
     return "".join(aa_seq), remainder
+
+
+def ncbi_genetic_code_to_dict(ncbi_string: str) -> Dict[str, str]:
+    """
+    Parse a translation table from NCBI in the five-line table format into a dictionary representation suitable for
+    :py:func:`fqfa.util.util.translate_dna`.
+
+    NCBI translation tables can be found
+    `here <https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=cgencodes>`_.
+
+    The default translation table is encoded by::
+
+        AAs  = FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG
+      Starts = ---M------**--*----M---------------M----------------------------
+      Base1  = TTTTTTTTTTTTTTTTCCCCCCCCCCCCCCCCAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGG
+      Base2  = TTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGG
+      Base3  = TCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAG
+
+
+    Information from the `Starts` line is not retained in the dictionary representation.
+
+    Blank lines or whitespace-only lines are automatically skipped, as are lines beginning with `#`.
+
+    Parameters
+    ----------
+    ncbi_string : str
+        Multi-line string containing a `transl_table` from NCBI.
+
+    Returns
+    -------
+    Dict[str, str]
+        Dictionary mapping codons to single-letter amino acid codes.
+
+    Raises
+    ------
+    TODO
+
+    """
+    lines = [
+        s.strip()
+        for s in ncbi_string.split("\n")
+        if len(s) > 0 and not s.startswith("#") and not s.isspace()
+    ]
+    if len(lines) != 5:
+        raise ValueError("transl_table string must have 5 lines")
+
+    transl_table = dict()
+    for s in lines:
+        try:
+            a, b = s.split("=")
+        except ValueError:
+            raise ValueError("transl_table line did not contain '=' separator")
+        transl_table[a.strip()] = b.strip()
+
+    if list(transl_table.keys()) != ["AAs", "Starts", "Base1", "Base2", "Base3"]:
+        raise ValueError("unmatched transl_table row labels")
+    if [len(s) for s in transl_table.values()] != [64] * 5:
+        raise ValueError("all transl_table rows must contain 64 characters")
+
+    codon_dict = dict()
+    for aa, codon in zip(
+        transl_table["AAs"],
+        (
+            "".join(nts)
+            for nts in zip(
+                transl_table["Base1"], transl_table["Base2"], transl_table["Base3"]
+            )
+        ),
+    ):
+        if codon not in codon_dict:
+            codon_dict[codon] = aa
+        else:
+            raise ValueError("all transl_table codons must be unique")
+
+    if sorted(codon_dict.keys()) != sorted(
+        "".join(nts) for nts in itertools.product("ACGT", repeat=3)
+    ):
+        raise ValueError("incomplete set of codons from transl_table")
+
+    return codon_dict
