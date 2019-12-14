313ff919614a6d78c22c204d12cefadeb3c0b2e6
Alan Rubin <alan.rubin@wehi.edu.au>
2019-12-14 22:29:34 +1100

    added fasta record generator function

--- tests/test_fasta.py
+++ tests/test_fasta.py
@@ -1,9 +1,52 @@
 import unittest
 
+from io import StringIO
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+from fqfa.fasta.fasta import yield_fasta_records
+
+
+class TestYieldFastaRecords(unittest.TestCase):
+    def test_empty(self):
+        data = StringIO("")
+
+        iterator = yield_fasta_records(data)
+
+        # should return an empty generator
+        self.assertRaises(StopIteration, next, iterator)
+
+    def test_noheader(self):
+        data = StringIO("ACGT\n")
+
+        iterator = yield_fasta_records(data)
+
+        # should return an empty generator
+        self.assertRaises(StopIteration, next, iterator)
+
+    def test_single(self):
+        data = StringIO(">seq1\nACGT\n")
+
+        iterator = yield_fasta_records(data)
+
+        self.assertTupleEqual(next(iterator), ("seq1", "ACGT"))
+        self.assertRaises(StopIteration, next, iterator)
+
+    def test_multiple(self):
+        data = StringIO(">seq1\nACGT\n>seq2\nTGCA\n\n>seq3\nTTTT")
+
+        iterator = yield_fasta_records(data)
+
+        self.assertTupleEqual(next(iterator), ("seq1", "ACGT"))
+        self.assertTupleEqual(next(iterator), ("seq2", "TGCA"))
+        self.assertTupleEqual(next(iterator), ("seq3", "TTTT"))
+        self.assertRaises(StopIteration, next, iterator)
+
+    def test_multiline(self):
+        data = StringIO(">seq1\nACGT\nTGCA")
+
+        iterator = yield_fasta_records(data)
+
+        self.assertTupleEqual(next(iterator), ("seq1", "ACGTTGCA"))
+        self.assertRaises(StopIteration, next, iterator)
 
 
 if __name__ == "__main__":
