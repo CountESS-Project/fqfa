9ee354b72f0f603585ce9b1db61d49f7026873e9
Alan Rubin <alan.rubin@wehi.edu.au>
2019-12-15 22:16:00 +1100

    added test stubs for fastqread

---
+++ tests/test_fastqread.py
@@ -0,0 +1,46 @@
+import unittest
+
+
+class TestFastqRead(unittest.TestCase):
+    def test_creation_no_errors(self):
+        self.assertEqual(True, False)
+
+    def test_creation_bad_header(self):
+        self.assertEqual(True, False)
+
+    def test_creation_bad_header2(self):
+        self.assertEqual(True, False)
+
+    def test_creation_length_mismatch(self):
+        self.assertEqual(True, False)
+
+    def test_creation_bad_bases(self):
+        self.assertEqual(True, False)
+
+    def test_creation_bad_quality(self):
+        self.assertEqual(True, False)
+
+    def test_length(self):
+        self.assertEqual(True, False)
+
+    def test_str(self):
+        self.assertEqual(True, False)
+
+    def test_average_quality(self):
+        self.assertEqual(True, False)
+
+    def test_min_quality(self):
+        self.assertEqual(True, False)
+
+    def test_trim(self):
+        self.assertEqual(True, False)
+
+    def test_trim_length(self):
+        self.assertEqual(True, False)
+
+    def test_reverse_complement(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == "__main__":
+    unittest.main()
