4db8576f010c583817565b8ac19093aa21e7d88b
Alan Rubin <alan.rubin@wehi.edu.au>
2019-12-17 10:57:45 +1100

    require backported dataclasses in Python 3.6

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 import setuptools
+import sys
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+requirements = []
+# require backported dataclasses in Python 3.6
+if sys.version_info.major == 3 and sys.version_info.minor == 6:
+    requirements.append("dataclasses")
 
 setuptools.setup(
     name="fqfa",
@@ -19,4 +25,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
+    install_requires=requirements,
 )
