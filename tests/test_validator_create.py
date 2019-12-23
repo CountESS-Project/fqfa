c0c0a2c9661d7fa3e0a11ce7a9d16b8ca552e652
Alan Rubin <alan.rubin@wehi.edu.au>
2019-12-23 11:10:24 +1100

    added tests for validator creation

--- tests/test_validator_create.py
+++ tests/test_validator_create.py
@@ -1,10 +1,76 @@
 import unittest
 
+from fqfa.validator.create import create_validator
+
 
 class TestCreateValidator(unittest.TestCase):
-    @unittest.expectedFailure
-    def test_something(self) -> None:
-        self.assertEqual(True, False)
+    def test_create_from_string(self) -> None:
+        # case sensitive
+        validator = create_validator("ACGT")
+
+        # test valid strings
+        self.assertIsNotNone(validator("ACGT"))
+        self.assertIsNotNone(validator("AAAAAAA"))
+
+        # test invalid strings
+        self.assertIsNone(validator("acgt"))
+        self.assertIsNone(validator("AAAAAAa"))
+        self.assertIsNone(validator(""))
+        self.assertIsNone(validator("123"))
+        self.assertIsNone(validator("AAAA AAA"))
+
+        # case insensitive
+        validator = create_validator("ACGT", case_sensitive=False)
+
+        # test valid strings
+        self.assertIsNotNone(validator("ACGT"))
+        self.assertIsNotNone(validator("AAAAAAA"))
+        self.assertIsNotNone(validator("acgt"))
+        self.assertIsNotNone(validator("AAAAAAa"))
+
+        # test invalid strings
+        self.assertIsNone(validator(""))
+        self.assertIsNone(validator("123"))
+        self.assertIsNone(validator("AAAA AAA"))
+
+    def test_create_from_list(self) -> None:
+        # case sensitive
+        validator = create_validator(list("ACGT"))
+
+        # test valid strings
+        self.assertIsNotNone(validator("ACGT"))
+        self.assertIsNotNone(validator("AAAAAAA"))
+
+        # test invalid strings
+        self.assertIsNone(validator("acgt"))
+        self.assertIsNone(validator("AAAAAAa"))
+        self.assertIsNone(validator(""))
+        self.assertIsNone(validator("123"))
+        self.assertIsNone(validator("AAAA AAA"))
+
+        # case insensitive
+        validator = create_validator(list("ACGT"), case_sensitive=False)
+
+        # test valid strings
+        self.assertIsNotNone(validator("ACGT"))
+        self.assertIsNotNone(validator("AAAAAAA"))
+        self.assertIsNotNone(validator("acgt"))
+        self.assertIsNotNone(validator("AAAAAAa"))
+
+        # test invalid strings
+        self.assertIsNone(validator(""))
+        self.assertIsNone(validator("123"))
+        self.assertIsNone(validator("AAAA AAA"))
+
+        # invalid list arguments
+        self.assertRaises(ValueError, create_validator, ["A", "C", "GT"])
+        self.assertRaises(
+            ValueError, create_validator, ["A", "C", "GT"], case_sensitive=False
+        )
+        self.assertRaises(ValueError, create_validator, ["A", "C", ""])
+        self.assertRaises(
+            ValueError, create_validator, ["A", "C", ""], case_sensitive=False
+        )
 
 
 if __name__ == "__main__":
