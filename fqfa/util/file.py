731fcc362ed810ac4432be2a0660becf3eed3d85
Alan Rubin <alan.rubin@wehi.edu.au>
2019-12-23 13:52:01 +1100

    tests and implementation for file utility functions

--- fqfa/util/file.py
+++ fqfa/util/file.py
@@ -2,8 +2,10 @@
 
 """
 
-from typing import TextIO
-
+import os
+import bz2
+import gzip
+from typing import Optional, TextIO
 
 _COMPRESSION_EXTENSIONS = [".bz2", ".gz"]
 """List[str]: list of recognized compression extensions.
@@ -11,29 +13,50 @@ _COMPRESSION_EXTENSIONS = [".bz2", ".gz"]
 """
 
 
-def open_compressed(path: str) -> TextIO:
+def open_compressed(path: str, encoding: Optional[str] = None) -> TextIO:
     """Open the file handle for reading using the correct (optional) decompression method.
 
     Compression status is determined by the file extension.
     Recognized file extensions are ``.bz2`` for bzip2 compression and ``.gz`` for gzip compression.
+    If there is any other file extension (or no extension), the file is opened normally.
     The file is opened in text mode.
 
     Parameters
     ----------
     path : str
         File path to be opened.
+    encoding : Optional[str]
+        Text file encoding as described for :py:class:`io.TextIOWrapper`.
 
     Returns
     -------
     TextIO
         Open file handle.
 
+    Raises
+    ------
+    FileNotFoundError
+        If path does not correspond to a file.
+    NotImplementedError
+        If a recognized compression extension lacks an implementation.
+
     """
-    pass
+    if not os.path.isfile(path):
+        raise FileNotFoundError("could not find file to open")
+    _, ext = os.path.splitext(path)
+    if ext.lower() in _COMPRESSION_EXTENSIONS:
+        if ext.lower == ".bz2":
+            return bz2.open(path, mode="rt", encoding=encoding)
+        elif ext.lower == ".gz":
+            return gzip.open(path, mode="rt", encoding=encoding)
+        else:  # pragma no cover
+            raise NotImplementedError("unsupported compression type")
+    else:
+        return open(path, mode="rt", encoding=encoding)
 
 
 def has_fastq_ext(path: str) -> bool:
-    """Checks whether the file has the expected file extension for FASTQ format.
+    """Checks whether the file path has the expected file extension for FASTQ format.
 
     Recognized file extensions are ``.fq`` and ``.fastq`` optionally in combination with a compression file extension
     supported by :py:func:`~fqfa.util.file.open_compressed`.
@@ -48,17 +71,18 @@ def has_fastq_ext(path: str) -> bool:
     bool
         True if the file has a recognized extension, else False.
 
-    Raises
-    ------
-    FileNotFoundError
-        If path does not correspond to a file.
-
     """
-    pass
+    root, ext = os.path.splitext(path)
+    if ext.lower() in _COMPRESSION_EXTENSIONS:
+        _, ext = os.path.splitext(root)
+    if ext.lower() in (".fq", ".fastq"):
+        return True
+    else:
+        return False
 
 
 def has_fasta_ext(path: str) -> bool:
-    """Checks whether the file has the expected file extension for FASTA format.
+    """Checks whether the file path has the expected file extension for FASTA format.
 
     Recognized file extensions are ``.fa`` and ``.fasta`` optionally in combination with a compression file extension
     supported by :py:func:`~fqfa.util.file.open_compressed`.
@@ -73,10 +97,11 @@ def has_fasta_ext(path: str) -> bool:
     bool
         True if the file has a recognized extension, else False.
 
-    Raises
-    ------
-    FileNotFoundError
-        If path does not correspond to a file.
-
     """
-    pass
+    root, ext = os.path.splitext(path)
+    if ext.lower() in _COMPRESSION_EXTENSIONS:
+        _, ext = os.path.splitext(root)
+    if ext.lower() in (".fa", ".fasta"):
+        return True
+    else:
+        return False
--- tests/test_util_file.py
+++ tests/test_util_file.py
@@ -1,5 +1,7 @@
 import unittest
 
+from fqfa.util.file import open_compressed, has_fastq_ext, has_fasta_ext
+
 
 class TestOpenCompressed(unittest.TestCase):
     @unittest.expectedFailure
@@ -8,15 +10,109 @@ class TestOpenCompressed(unittest.TestCase):
 
 
 class TestHasFastqExt(unittest.TestCase):
-    @unittest.expectedFailure
-    def test_something(self) -> None:
-        self.assertEqual(True, False)
+    def test_uncompressed(self) -> None:
+        self.assertTrue(has_fastq_ext("file.fq"))
+        self.assertTrue(has_fastq_ext("file.fastq"))
+
+        self.assertFalse(has_fastq_ext("file"))
+        self.assertFalse(has_fastq_ext(".dotfile"))
+        self.assertFalse(has_fastq_ext("file.txt"))
+
+        self.assertTrue(has_fastq_ext("file.fq".upper()))
+        self.assertTrue(has_fastq_ext("file.fastq".upper()))
+
+        self.assertTrue(has_fastq_ext("rel/path/to/file.fq"))
+        self.assertTrue(has_fastq_ext("rel/path/to/file.fastq"))
+
+        self.assertTrue(has_fastq_ext("/abs/path/to/file.fq"))
+        self.assertTrue(has_fastq_ext("/abs/path/to/file.fastq"))
+
+    def test_compressed_bzip2(self) -> None:
+        self.assertTrue(has_fastq_ext("file.fq.bz2"))
+        self.assertTrue(has_fastq_ext("file.fastq.bz2"))
+
+        self.assertFalse(has_fastq_ext("file.bz2"))
+        self.assertFalse(has_fastq_ext(".dotfile.bz2"))
+        self.assertFalse(has_fastq_ext("file.txt.bz2"))
+
+        self.assertTrue(has_fastq_ext("file.fq.bz2".upper()))
+        self.assertTrue(has_fastq_ext("file.fastq.bz2".upper()))
+
+        self.assertTrue(has_fastq_ext("rel/path/to/file.fq.bz2"))
+        self.assertTrue(has_fastq_ext("rel/path/to/file.fastq.bz2"))
+
+        self.assertTrue(has_fastq_ext("/abs/path/to/file.fq.bz2"))
+        self.assertTrue(has_fastq_ext("/abs/path/to/file.fastq.bz2"))
+
+    def test_compressed_gzip(self) -> None:
+        self.assertTrue(has_fastq_ext("file.fq.gz"))
+        self.assertTrue(has_fastq_ext("file.fastq.gz"))
+
+        self.assertFalse(has_fastq_ext("file.gz"))
+        self.assertFalse(has_fastq_ext(".dotfile.gz"))
+        self.assertFalse(has_fastq_ext("file.txt.gz"))
+
+        self.assertTrue(has_fastq_ext("file.fq.gz".upper()))
+        self.assertTrue(has_fastq_ext("file.fastq.gz".upper()))
+
+        self.assertTrue(has_fastq_ext("rel/path/to/file.fq.gz"))
+        self.assertTrue(has_fastq_ext("rel/path/to/file.fastq.gz"))
+
+        self.assertTrue(has_fastq_ext("/abs/path/to/file.fq.gz"))
+        self.assertTrue(has_fastq_ext("/abs/path/to/file.fastq.gz"))
 
 
 class TestHasFastaExt(unittest.TestCase):
-    @unittest.expectedFailure
-    def test_something(self) -> None:
-        self.assertEqual(True, False)
+    def test_uncompressed(self) -> None:
+        self.assertTrue(has_fasta_ext("file.fa"))
+        self.assertTrue(has_fasta_ext("file.fasta"))
+
+        self.assertFalse(has_fasta_ext("file"))
+        self.assertFalse(has_fasta_ext(".dotfile"))
+        self.assertFalse(has_fasta_ext("file.txt"))
+
+        self.assertTrue(has_fasta_ext("file.fa".upper()))
+        self.assertTrue(has_fasta_ext("file.fasta".upper()))
+
+        self.assertTrue(has_fasta_ext("rel/path/to/file.fa"))
+        self.assertTrue(has_fasta_ext("rel/path/to/file.fasta"))
+
+        self.assertTrue(has_fasta_ext("/abs/path/to/file.fa"))
+        self.assertTrue(has_fasta_ext("/abs/path/to/file.fasta"))
+
+    def test_compressed_bzip2(self) -> None:
+        self.assertTrue(has_fasta_ext("file.fa.bz2"))
+        self.assertTrue(has_fasta_ext("file.fasta.bz2"))
+
+        self.assertFalse(has_fasta_ext("file.bz2"))
+        self.assertFalse(has_fasta_ext(".dotfile.bz2"))
+        self.assertFalse(has_fasta_ext("file.txt.bz2"))
+
+        self.assertTrue(has_fasta_ext("file.fa.bz2".upper()))
+        self.assertTrue(has_fasta_ext("file.fasta.bz2".upper()))
+
+        self.assertTrue(has_fasta_ext("rel/path/to/file.fa.bz2"))
+        self.assertTrue(has_fasta_ext("rel/path/to/file.fasta.bz2"))
+
+        self.assertTrue(has_fasta_ext("/abs/path/to/file.fa.bz2"))
+        self.assertTrue(has_fasta_ext("/abs/path/to/file.fasta.bz2"))
+
+    def test_compressed_gzip(self) -> None:
+        self.assertTrue(has_fasta_ext("file.fa.gz"))
+        self.assertTrue(has_fasta_ext("file.fasta.gz"))
+
+        self.assertFalse(has_fasta_ext("file.gz"))
+        self.assertFalse(has_fasta_ext(".dotfile.gz"))
+        self.assertFalse(has_fasta_ext("file.txt.gz"))
+
+        self.assertTrue(has_fasta_ext("file.fa.gz".upper()))
+        self.assertTrue(has_fasta_ext("file.fasta.gz".upper()))
+
+        self.assertTrue(has_fasta_ext("rel/path/to/file.fa.gz"))
+        self.assertTrue(has_fasta_ext("rel/path/to/file.fasta.gz"))
+
+        self.assertTrue(has_fasta_ext("/abs/path/to/file.fa.gz"))
+        self.assertTrue(has_fasta_ext("/abs/path/to/file.fasta.gz"))
 
 
 if __name__ == "__main__":
