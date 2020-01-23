8f85d0d0f5308ed1247149ca5c1b224b5d0c5b66
Alan Rubin <alan.rubin@wehi.edu.au>
2020-01-23 16:10:11 +1100

    added a stub for PE read generator function

--- fqfa/fastq/fastq.py
+++ fqfa/fastq/fastq.py
@@ -35,3 +35,36 @@ def yield_fastq_reads(handle: TextIO) -> Generator[FastqRead, None, None]:
         else:
             lines = [x.rstrip() for x in lines]  # remove trailing newlines
             yield FastqRead(*lines)
+
+
+def yield_fastq_reads_pe(
+    handle_fwd: TextIO, handle_rev: TextIO, revcomp: bool = False
+) -> Generator[Tuple[FastqRead, FastqRead], None, None]:
+    """Generator function that returns FASTQ read pairs as a tuple of objects.
+
+    Parameters
+    ----------
+    handle_fwd : TextIO
+        Open text file handle to parse for forward reads.
+
+    handle_rev : TextIO
+        Open text file handle to parse for reverse reads.
+
+    revcomp : bool
+        Whether to reverse-complement the reverse reads. Default False.
+
+    Returns
+    -------
+    Tuple[FastqRead, FastqRead]
+        Tuple of forward and reverse FastqRead objects.
+
+    Raises
+    ------
+    ValueError
+        If a record is incomplete.
+    ValueError
+        If the read header portion before the first whitespace doesn't match between read pairs.
+        This usually contains the machine ID and read coordinates, and is therefore expected to match for PE data.
+
+    """
+    pass
