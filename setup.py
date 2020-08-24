d05139e075ae09f60f368edf0c74e58348a2b491
Alan Rubin <alan.rubin@wehi.edu.au>
2020-08-25 00:36:15 +1000

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.version_info.major == 3 and sys.version_info.minor == 6:
 
 setuptools.setup(
     name="fqfa",
-    version="1.0.0",
+    version="1.1.0",
     author="Alan F Rubin",
     author_email="alan.rubin@wehi.edu.au",
     description="A lightweight Python library for handling FASTQ and FASTA files.",
