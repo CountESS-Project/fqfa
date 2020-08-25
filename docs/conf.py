b332f7d78973d5db882265d12e956273afa5d519
Alan Rubin <alan.rubin@wehi.edu.au>
2020-08-25 14:55:00 +1000

    increment version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2019-2020, Alan F Rubin"
 author = "Alan F Rubin"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = "1.1.0"
 
 
 # -- General configuration ---------------------------------------------------
